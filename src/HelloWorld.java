987b90c76e92e79f8229452cb45ce3b21ce29158
vinayakashegde1999 <61392116+vinayakashegde1999@users.noreply.github.com>
2020-03-04 11:45:59 +0000

    Update HelloWorld.java

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,14 @@
 public class HelloWorld {
-
+    
+    public static void vulnerablecode(){
+    StringBuilder query = new StringBuilder();
+query.append( "select * from user u where u.name in (" + namesString + ")" );
+try {
+	Connection connection = getConnection();
+    Statement statement = connection.createStatement();
+    resultSet = statement.executeQuery(query.toString());
+}
+    }
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
         System.out.println("Hello, World");
